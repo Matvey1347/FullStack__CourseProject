29655d728a35cab94610bd19657b04c358dad923
мак <mak@MacBook-Pro-mak.local>
2022-11-30 13:48:01 +0200

    d

--- client/src/app/shared/layouts/site-layout/site-layout.links.ts
+++
@@ -1,7 +0,0 @@
-export const SiteLayoutLinks = [
-  {url: '/overview', name: 'Обзор'},
-  {url: '/analytics', name: 'Аналитика'},
-  {url: '/history', name: 'История'},
-  {url: '/order', name: 'Добавить заказ'},
-  {url: '/categories', name: 'Ассортимент'},
-]
--- config/keys.dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://Matvey:ykqXD8o0S2oTJTuw@cluster0.0zwk9y3.mongodb.net/?retryWrites=true&w=majority',
-  jwt: 'dev-jwt'
-}
